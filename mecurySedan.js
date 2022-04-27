33028b5ab0e2c2c1e8e83bd5e10cf5d899239581
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-26 22:09:53 -0400

    added class extendor

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,31 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+const VehicleModule = require("./vehicle");
 
+class VehicleProperties extends VehicleModule {
+  constructor(
+    maximumPassengers,
+    passenger,
+    numberOfWheels,
+    maximumSpeed,
+    fuel,
+    scheduleService
+  ) {
+    this.maximumPassengers;
+    this.passenger;
+    this.numberOfWheels;
+    this.maximumSpeed;
+    this.fuel;
+    this.scheduleService;
+  }
+  loadPassenger() {}
+  scheduleService(){}
+}
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let v = new VehicleModule.Vehicle(
+  "Mecury",
+  "Sedan",
+  "1965",
+  "color",
+  "mileage"
+);
+console.log(v.make);
