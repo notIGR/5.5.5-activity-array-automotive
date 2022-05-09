7f3ca6d9e2b0cf4d8d59a23778c7f7e04ffa488f
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-09 17:21:21 -0400

    formatting

--- mecurySedan.js
+++ mecurySedan.js
@@ -2,34 +2,35 @@
 const VehicleModule = require("./vehicle");
 
 class Car extends VehicleModule {
-    constructor(make, model, year, color, mileage) {
-        super(make, model, year, color, mileage);
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
     this.maximumPassengers = 5;
     this.passenger = 0;
-    this.numberOfWheels= 4;
+    this.numberOfWheels = 4;
     this.maximumSpeed = 200;
     this.fuel = 100;
     this.scheduleService = false;
   }
   maitLight() {
-    if(this.mileage >= 10000) {
-        this.scheduleService = true
-        //console.log('Schedule Service') i dont have anyt vale at the end of this function
-        return this.scheduleService;
+    if (this.mileage >= 10000) {
+      this.scheduleService = true;
+      //console.log('Schedule Service') i dont have anyt vale at the end of this function
+      return this.scheduleService;
     }
   }
   loadPassenger(num) {
-      if(this.passenger < this.maximumPassengers) {
-          if((num + this.passenger) <= this.maximumPassengers) {
-              this.passenger = num
-              return this.passenger;
-              //console.log(this.passenger) again, no value after function runs... whateever number is entered BECOMES this.passagner, then checks against this.max
-          } 
-          else {
-          console.log(this.make + " "+ this.make + 'will not fit that many passengers.')
+    if (this.passenger < this.maximumPassengers) {
+      if (num + this.passenger <= this.maximumPassengers) {
+        this.passenger = num;
+        return this.passenger;
+        //console.log(this.passenger) again, no value after function runs... whateever number is entered BECOMES this.passagner, then checks against this.max
+      } else {
+        console.log(
+          this.make + " " + this.make + "will not fit that many passengers."
+        );
       }
+    }
   }
-}
 }
 //this shows how to call from this module...
 //need to add new "constructor" values?
@@ -39,7 +40,7 @@ let v = new VehicleModule.Vehicle(
   "1965",
   "dirt bag brown",
   "10001"
-)
+);
 
 v.start(); //already defined in vehicle.js
 v.stop(); //already defined in vehicle.js
